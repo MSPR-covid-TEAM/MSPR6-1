9a4642c35fb9f79493d7a527b32bd0a732972972
DxrkSquls <bleuzehugo.work@gmail.com>
2024-12-05 10:36:22 +0100

    test

---
+++ app/Models/Pandemie.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pandemie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nom', 'description', 'date_debut', 'date_fin', 'statut'];
+
+    // Relation : Une pandémie a plusieurs données épidémiologiques
+    public function donneesEpidemiologiques()
+    {
+        return $this->hasMany(DonneesEpidemiologiques::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\dashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('dashboard');
+// });
+
+Route::get('/', [dashboardController::class, 'index']);
